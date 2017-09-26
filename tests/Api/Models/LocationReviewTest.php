d3d7508027a146979f55371e03fbda563372e0fc
Michael-James Parsons <mjay.parsons@gmail.com>
2017-09-26 11:43:32 -0400

    Added location tests

---
+++ tests/Api/Models/LocationReviewTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace Api\Models;
+
+use MetroPublisher\Api\Models\LocationReview;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class LocationReviewTest
+ * @package Api\Models
+ */
+class LocationReviewTest extends TestCase
+{
+    public function testMetaFields() {
+        $expected = [
+            'location_uuid',
+            'rating',
+            'content',
+            'meta_title',
+            'meta_description',
+            'print_description',
+            'evergreen',
+            'teaser_image_uuid',
+            'feature_image_uuid'
+        ];
+
+        $this->assertEquals($expected, LocationReview::getMetaFields());
+    }
+}
