fe58d8b72791f0d0792c45cd39a1622a8cb11db7
Michael-James Parsons <mjay.parsons@gmail.com>
2017-09-26 11:28:21 -0400

    Added ExternalSlotMedia tests

---
+++ tests/Api/Models/ExternalSlotMediaTest.php
@@ -0,0 +1,36 @@
+<?php
+namespace Api\Models;
+
+use MetroPublisher\Api\Models\ExternalSlotMedia;
+use MetroPublisher\Api\Models\Slot;
+use MetroPublisher\Api\Models\SlotMedia;
+use MetroPublisher\MetroPublisher;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ExternalSlotMediaTest
+ * @package Api\Models
+ */
+class ExternalSlotMediaTest extends TestCase
+{
+    public function testSlotMediaType() {
+        /** @var MetroPublisher $mockMetroPublisher */
+        $mockMetroPublisher = $this->createMock(MetroPublisher::class);
+        /** @var Slot $mockSlot */
+        $mockSlot = $this->createMock(Slot::class);
+
+        $externalMedia = new ExternalSlotMedia($mockMetroPublisher, $mockSlot);
+        $this->assertEquals(SlotMedia::TYPE_EXTERNAL_URL, $externalMedia->getType());
+    }
+
+    public function testMediaFields() {
+        $expected = [
+            'url_type',
+            'url',
+            'urls',
+            'content'
+        ];
+
+        $this->assertEquals($expected, ExternalSlotMedia::getMetaFields());
+    }
+}
