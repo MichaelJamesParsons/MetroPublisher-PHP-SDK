1a2fed51ba65e8191bad8c4e9be38b341c4e6ea4
= <mparsons@elinkdesign.com>
2017-10-25 16:09:29 -0400

    Fix for event occurrence support

--- src/Api/Collections/EventCollection.php
+++ src/Api/Collections/EventCollection.php
@@ -44,14 +44,15 @@ class EventCollection extends AbstractResourceCollection implements ResourceColl
      * @return \MetroPublisher\Api\Models\AbstractModel[]
      */
     public function getOccurrences(DateTime $start = null, DateTime $end = null, $page = 1) {
-        $occurrences = $this->context->get('/events', [
+        $occurrences = $this->context->get('/events/occurrences', [
             'period' => sprintf('%s_%s', $start->format('Y-m-d'), $end->format('Y-m-d')),
-            'page'   => $page
+            'page'   => $page,
+            'fields' => implode('-', EventOccurrence::getDefaultFields())
         ]);
 
         return ModelDeserializer::convertCollection(
             new ModelResolver(EventOccurrence::class),
-            $occurrences,
+            $occurrences['items'],
             [$this->context]
         );
     }
--- src/Api/Models/EventOccurrence.php
+++ src/Api/Models/EventOccurrence.php
@@ -6,19 +6,84 @@ use DateTime;
 /**
  * Class EventOccurrence
  * @package MetroPublisher\Api\Models
- *
- * @property string $event_uuid
- * @property DateTime $start_time
- * @property DateTime $end_time
  */
 class EventOccurrence extends AbstractModel
 {
+    /** @var  string */
+    protected $event_uuid;
+
+    /** @var  DateTime */
+    protected $dtstart;
+
+    /** @var DateTime */
+    protected $dtend;
+
+    /**
+     * @return string
+     */
+    public function getEventUuid()
+    {
+        return $this->event_uuid;
+    }
+
+    /**
+     * @param string $event_uuid
+     *
+     * @return $this
+     */
+    public function setEventUuid($event_uuid)
+    {
+        $this->event_uuid = $event_uuid;
+
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getDtstart()
+    {
+        return $this->dtstart;
+    }
+
+    /**
+     * @param DateTime $dtstart
+     *
+     * @return $this
+     */
+    public function setDtstart($dtstart)
+    {
+        $this->dtstart = $dtstart;
+
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getDtend()
+    {
+        return $this->dtend;
+    }
+
+    /**
+     * @param DateTime $dtend
+     *
+     * @return $this
+     */
+    public function setDtend($dtend)
+    {
+        $this->dtend = $dtend;
+
+        return $this;
+    }
+
     public static function getDefaultFields()
     {
-        return [
+        return array(
             'event_uuid',
-            'start_time',
-            'end_time'
-        ];
+            'dtstart',
+            'dtend'
+        );
     }
 }
